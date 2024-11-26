78482e4de82aabae8032f30230d6922719e9218b
quyenpt-99 <phanquyen10d@gmail.com>
2024-11-26 10:57:32 +0700

    Trang chi tiết sản phẩm

---
+++ views/chitietsanpham.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Cache-control" content="public" />
+    <title>Áo gió có mũ nam MGF002W3</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="https://savani.vn/modules/products/assets/css/detail.css?v=8.65" />
+    <link rel="stylesheet" type="text/css" media="screen" href="https://savani.vn/templates/default/css/bootstrap.min.css?v=8.65" />
+    <link rel="stylesheet" type="text/css" media="screen" href="https://savani.vn/templates/default/scss/font-awesome/css/font-awesome.css?v=8.65" />
+    <link rel="stylesheet" type="text/css" media="screen" href="https://savani.vn/templates/default/css/mmenu.css?v=8.65" />
+    <link rel="stylesheet" type="text/css" media="screen" href="https://savani.vn/templates/default/css/style.css?v=8.65" />
+    <link rel="stylesheet" type="text/css" media="screen" href="https://savani.vn/blocks/mainmenu/assets/css/styles.css?v=8.65" />
+</head>
+
+<body>
+    <div id="myPage">
+
+        <?php require_once 'khung/header.php'; ?>
+
+        <main id="main">
+            <div class="container">
+                <div class="fix-breadcrum">
+                    <div class="container">
+                        <div class='breadcrumbs row-item'>
+                            <!--    <div class="container">-->
+                            <ol class="breadcrumb row-item">
+                                <li class='breadcrumb-item breadcumbs-first'>
+                                    <a title='Savani' href="https://savani.vn/" rel='nofollow'>
+                                        Trang chủ </a>
+                                </li>
+                                <li class="breadcrumb-item ">
+                                    /
+                                    <a href="https://savani.vn/ao-khoac-ao-gio-pc8.html" title="Áo khoác áo gió">
+                                        mã - <?= $sanpham['id_sanpham'] ?>
+                                    </a>
+                                </li>
+                                <li class="breadcrumb-item active">
+                                    /
+                                    <?= $sanpham['name'] ?>
+                                </li>
+                            </ol>
+                        </div><!-- END: .breadcrumb -->
+                        <div class="clear"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="outer-top">
+                    <div class="top-info row no-gutters ">
+                        <div class="col-md-6 no-gutters image-box-show">
+                            <div class="column-image clearfix hidden1">
+
+                                <div class="img-show" style="position: relative;margin-right:20px">
+                                    <img src="./assets/img/<?= $sanpham['image'] ?>" alt="Sản phẩm 1" class="hinh-anh-san-pham">
+
+                                    <!-- <img src="https://savani.vn/images/products/2023/12/15/large/ao-gio-nam-mgf002w3-2-k01_1702625718.jpg" alt="Áo gió có mũ nam MGF002W3"> -->
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div class="col-md-6 no-gutters prod-info">
+                            <p class="prod-name"><?= $sanpham['name'] ?></p>
+                            <div class="info">
+                                <p class="model">Mã sản phẩm: <span>#<?= $sanpham['id_sanpham'] ?></span></p>
+                                <!-- <span class="status">Tình trạng: Còn hàng</span> -->
+                            </div>
+
+                            <div class="info-login">
+                                <div class="box-log box-logged">
+
+
+                                    <div class="price-by-size">
+                                        <div class="promo">
+                                            <div class="promo_config">
+                                                <p><em><span style="color:#3498db;"><strong>Freeship với đơn hàng từ 500k</strong></span></em></p>
+                                            </div>
+                                        </div>
+                                        <div class="color">
+                                            <h2 class="row-title-left">Giá: <span class="chose-color"><?= $sanpham['price'] ?>đ</span>
+                                            </h2>
+                                        </div>
+
+                                        <!-- THEM VAO GIO HANG -->
+
+                                        <div class="quant">
+                                            <p class="row-title-left">Số lượng</p>
+                                            <div class="count-box">
+                                                <div class="count-number">
+                                                    <div class="toggle-quantity">
+                                                        <button id="decrement" class="decrement ml-1">-</button>
+                                                    </div>
+                                                    <input id="quantity" class="quantity" type="text" value="1" disabled />
+
+                                                    <div class="toggle-quantity">
+                                                        <button id="increment" class="increment mr-1">+</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="buy">
+                                            <div class="boxAddCart">
+                                                <div class="add-to-cart ripple-btn no-gutters" data-id="1535">
+                                                    <button class="addCart active" id="addToCartButton" aria-label="Add product to cart">
+                                                        <div style="display:flex;gap:8px;justify-content:center">
+                                                            <svg width="22" height="22" viewBox="0 2 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                <path d="M5.0415 20.1667C4.67484 20.1667 4.354 20.0292 4.079 19.7542C3.804 19.4792 3.6665 19.1584 3.6665 18.7917V6.87504C3.6665 6.50837 3.804 6.18754 4.079 5.91254C4.354 5.63754 4.67484 5.50004 5.0415 5.50004H7.56234V5.27087C7.56234 4.30837 7.89463 3.49483 8.55921 2.83025C9.2238 2.16567 10.0373 1.83337 10.9998 1.83337C11.9623 1.83337 12.7759 2.16567 13.4405 2.83025C14.105 3.49483 14.4373 4.30837 14.4373 5.27087V5.50004H16.9582C17.3248 5.50004 17.6457 5.63754 17.9207 5.91254C18.1957 6.18754 18.3332 6.50837 18.3332 6.87504V18.7917C18.3332 19.1584 18.1957 19.4792 17.9207 19.7542C17.6457 20.0292 17.3248 20.1667 16.9582 20.1667H5.0415ZM5.0415 18.7917H16.9582V6.87504H14.4373V8.93754C14.4373 9.13233 14.3711 9.29561 14.2385 9.42738C14.106 9.55915 13.9417 9.62504 13.7458 9.62504C13.5499 9.62504 13.387 9.55915 13.2571 9.42738C13.1273 9.29561 13.0623 9.13233 13.0623 8.93754V6.87504H8.93734V8.93754C8.93734 9.13233 8.87107 9.29561 8.73854 9.42738C8.60599 9.55915 8.44175 9.62504 8.24583 9.62504C8.04989 9.62504 7.88699 9.55915 7.75713 9.42738C7.62727 9.29561 7.56234 9.13233 7.56234 8.93754V6.87504H5.0415V18.7917ZM8.93734 5.50004H13.0623V5.27087C13.0623 4.69032 12.8637 4.20143 12.4665 3.80421C12.0693 3.40699 11.5804 3.20837 10.9998 3.20837C10.4193 3.20837 9.93039 3.40699 9.53317 3.80421C9.13595 4.20143 8.93734 4.69032 8.93734 5.27087V5.50004Z" fill="#E2231A" />
+                                                            </svg>
+                                                            <span>Thêm vào giỏ hàng</span>
+                                                        </div>
+                                                        <!-- </a> -->
+                                                    </button>
+                                                </div>
+                                            </div>
+
+                                            <div class="buy-now">
+                                                <div class="add-to-cart-now ripple-btn no-gutters" data-id="1535">
+                                                    <a onclick="addCartNow(4408)" id="addCartNow_page1" class="addCartNow active">
+                                                        <div style="display:flex;gap:8px;justify-content:center">
+                                                            <svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                <path d="M5.5415 20.1667C5.17484 20.1667 4.854 20.0292 4.579 19.7542C4.304 19.4792 4.1665 19.1584 4.1665 18.7917V6.87504C4.1665 6.50837 4.304 6.18754 4.579 5.91254C4.854 5.63754 5.17484 5.50004 5.5415 5.50004H8.06234V5.27087C8.06234 4.30837 8.39463 3.49483 9.05921 2.83025C9.7238 2.16567 10.5373 1.83337 11.4998 1.83337C12.4623 1.83337 13.2759 2.16567 13.9405 2.83025C14.605 3.49483 14.9373 4.30837 14.9373 5.27087V5.50004H17.4582C17.8248 5.50004 18.1457 5.63754 18.4207 5.91254C18.6957 6.18754 18.8332 6.50837 18.8332 6.87504V18.7917C18.8332 19.1584 18.6957 19.4792 18.4207 19.7542C18.1457 20.0292 17.8248 20.1667 17.4582 20.1667H5.5415ZM5.5415 18.7917H17.4582V6.87504H14.9373V8.93754C14.9373 9.13233 14.8711 9.29561 14.7385 9.42738C14.606 9.55915 14.4417 9.62504 14.2458 9.62504C14.0499 9.62504 13.887 9.55915 13.7571 9.42738C13.6273 9.29561 13.5623 9.13233 13.5623 8.93754V6.87504H9.43734V8.93754C9.43734 9.13233 9.37107 9.29561 9.23854 9.42738C9.10599 9.55915 8.94175 9.62504 8.74583 9.62504C8.54989 9.62504 8.38699 9.55915 8.25713 9.42738C8.12727 9.29561 8.06234 9.13233 8.06234 8.93754V6.87504H5.5415V18.7917ZM9.43734 5.50004H13.5623V5.27087C13.5623 4.69032 13.3637 4.20143 12.9665 3.80421C12.5693 3.40699 12.0804 3.20837 11.4998 3.20837C10.9193 3.20837 10.4304 3.40699 10.0332 3.80421C9.63595 4.20143 9.43734 4.69032 9.43734 5.27087V5.50004Z" fill="white" />
+                                                            </svg>
+                                                            <span>Mua ngay</span>
+                                                        </div>
+                                                    </a>
+
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Gioi thieu san pham -->
+
+
+                <div class="content">
+
+                    <div class="outer-detail">
+                        <div class="row">
+                            <div class="col-md-9 no-gutters row-detail">
+                                <h3 class="title_info">Thông tin chi tiết</h3>
+                                <p class="title-info">Mô tả chi tiết về sản phẩm</p>
+                                <p class="item-info"><span class="left-tag">Thương hiệu</span><span class="right-tag">Savani</span></p>
+                                <p class="item-info"><span class="left-tag">Mã sản phẩm</span><span class="right-tag">MGF002W3</span></p>
+                                <p class="item-info"><span class="left-tag">Chất liệu</span><span class="right-tag">100% polyester</span></p>
+                                <p class="item-info"><span class="left-tag">Xuất xứ</span><span class="right-tag">Việt Nam</span></p>
+                                <p class="item-info"><span class="left-tag">Kiểu dáng</span><span class="right-tag">Slim fit</span></p>
+                                <p class="item-info"><span class="left-tag">Ưu điểm</span><span class="right-tag">Cản gió, chống bụi. Khả năng chống thấm nước và giữ ấm cực tốt</span></p>
+                                <div class="content">
+                                </div>
+                            </div>
+                            <div class="col-md-3 no-gutters row-detail">
+                                <div class="suppory-product">
+                                    <p style="font-weight: bold;font-size: 16px;line-height: 19px;"><strong>TỔNG ĐÀI HỖ TRỢ</strong></p>
+
+                                    <p><img alt="" src="/upload_images/images/2022/01/14/dt.jpg" style="margin-right: 10px; width: 16px; height: 17px;" />Hotline mua hàng:&nbsp;<strong>0888566599</strong></p>
+
+                                    <p><img alt="" src="/upload_images/images/2022/01/14/dt.jpg" style="margin-right: 10px; width: 16px; height: 17px;" />Hotline hỗ trợ, khiếu nại: <strong>0888566599</strong></p>
+
+                                    <p><img alt="" src="/upload_images/images/2022/01/14/tg.png" style="margin-right: 10px; width: 16px; height: 17px;" />Thời gian làm việc từ 8h00 - 22h00 (thứ 2 - chủ nhật)</p>
+
+                                    <!--    relate-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer class="footer">
+            <?php require_once 'views/khung/footer.php'; ?>
+        </footer>
+    </div>
+
+
+    <script>
+        const quantityInput = document.getElementById('quantity');
+        const incrementButton = document.getElementById('increment');
+        const decrementButton = document.getElementById('decrement');
+        const addToCartButton = document.getElementById('addToCartButton');
+
+
+        //  tăng giảm số lượng sản phẩm
+        incrementButton.addEventListener('click', () => {
+            let quantity = parseInt(quantityInput.value);
+            quantity++;
+            quantityInput.value = quantity;
+        });
+
+        decrementButton.addEventListener('click', () => {
+            let quantity = parseInt(quantityInput.value);
+            if (quantity > 1) {
+                quantity--;
+                quantityInput.value = quantity;
+            }
+        });
+
+        // lấy giá trị trong local 
+        function getCartItemsFromLocalStorage() {
+            const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+            return cartItems;
+        }
+
+
+        // thêm sản phẩm vào local 
+        function addToCartLocal(newCartItem) {
+            let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+            cartItems.push(newCartItem);
+            localStorage.setItem('cartItems', JSON.stringify(cartItems))
+        }
+
+        addToCartButton.addEventListener('click', () => {
+            const productId = <?= $sanpham['id_sanpham'] ?>;
+            const productName = `<?= $sanpham['name'] ?>`;
+            const productPrice = <?= $sanpham['price'] ?>;
+            const productImage = `<?= $sanpham['image'] ?>`;
+
+            const newCartItem = {
+                quantity: parseInt(quantityInput.value),
+                id: productId,
+                name: productName,
+                price: productPrice,
+                image: productImage,
+                total: parseInt(quantityInput.value) * productPrice
+            };
+
+            const existsCart = getCartItemsFromLocalStorage();
+
+            // Check if the item already exists in the cart
+            let itemExists = false;
+            existsCart.forEach((item, index) => {
+                if (item.id === newCartItem.id) {
+                    item.quantity += newCartItem.quantity;
+                    itemExists = true;
+                    existsCart[index] = item; // Update the item in the array
+                    alert('Cập nhật số lượng thành công');
+
+                }
+            });
+
+            if (!itemExists) {
+                existsCart.push(newCartItem);
+                alert('Thêm sản phẩm vào giỏ hàng thành công');
+                window.location.reload();
+            }
+            localStorage.setItem('cartItems', JSON.stringify(existsCart));
+
+        });
+    </script>
+
+</body>
+
+</html>
